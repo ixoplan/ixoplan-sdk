c0472c9521e08111d4d55e1153bf63f291b2c701
Janos Pasztor <janos@pasztor.at>
2016-12-30 11:46:33 +0100

    Fixed HTTP request

--- src/Request/HTTPRequestClient.php
+++ src/Request/HTTPRequestClient.php
@@ -2,6 +2,7 @@
 
 namespace Ixolit\Dislo\Request;
 
+use Ixolit\Dislo\Exceptions\DisloException;
 use Ixolit\Dislo\HTTP\HTTPClientAdapter;
 
 /**
@@ -62,23 +63,25 @@ class HTTPRequestClient implements RequestClient {
 	 *
 	 * @return array
 	 *
+	 * @throws DisloException
 	 * @throws InvalidResponseData
 	 */
 	public function request($uri, array $params) {
 		$payload   = \json_encode($params);
 		$uri       = $this->httpClient->createUri()
-									  ->withScheme('https:')
-									  ->withHost($this->host)
-									  ->withPath($uri)
-									  ->withQuery(
-										  'api_key=' . \urlencode($this->apiKey) .
-										  '&timestamp=' . \urlencode($this->timeProvider->getTimestamp()));
-		$signature = \hash_hmac('MD5', $uri . $payload, $this->apiSecret);
+		                              ->withScheme('https')
+		                              ->withHost($this->host)
+		                              ->withPath($uri)
+		                              ->withQuery(
+			                              'api_key=' . \urlencode($this->apiKey) .
+			                              '&timestamp=' . \urlencode($this->timeProvider->getTimestamp()));
+		$signature = \hash_hmac('MD5', $uri->getPath() . '?' . $uri->getQuery() . $payload, $this->apiSecret);
 		$uri       = $uri->withQuery($uri->getQuery() . '&signature=' . \urlencode($signature));
 		$request   = $this->httpClient->createRequest()
-									  ->withUri($uri)
-									  ->withMethod('POST')
-									  ->withHeader('Content-Type', 'text/json');
+		                              ->withUri($uri)
+		                              ->withMethod('POST')
+		                              ->withHeader('Content-Type', 'text/json')
+		                              ->withBody($this->httpClient->createStringStream($payload));
 
 		$response = $this->httpClient->send($request);
 
