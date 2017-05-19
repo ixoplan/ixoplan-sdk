b74403d67f5f30240f6a1c07692ce268b21711e4
Markus Marchewa <m.marchewa@ixolit.com>
2017-05-19 16:59:46 +0200

    added RequestClientEx

--- src/Request/HTTPRequestClient.php
+++ src/Request/HTTPRequestClient.php
@@ -4,13 +4,16 @@ namespace Ixolit\Dislo\Request;
 
 use Ixolit\Dislo\Exceptions\DisloException;
 use Ixolit\Dislo\HTTP\HTTPClientAdapter;
+use Ixolit\Dislo\HTTP\HTTPClientAdapterEx;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
 
 /**
  * This client uses a PSR-7 client to talk to Dislo.
  *
  * @package Dislo
  */
-class HTTPRequestClient implements RequestClient {
+class HTTPRequestClient implements RequestClient, RequestClientEx {
 
 	/**
 	 * @var HTTPClientAdapter
@@ -32,6 +35,47 @@ class HTTPRequestClient implements RequestClient {
 	 * @var
 	 */
 	private $apiSecret;
+
+	/**
+	 * @return HTTPClientAdapterEx
+	 *
+	 * @throws \Exception
+	 */
+	private function getHttpClientEx() {
+		if ($this->httpClient instanceof HTTPClientAdapterEx) {
+			return $this->httpClient;
+		}
+		else {
+			// TODO: specific exception
+			throw new \Exception('not implemented');
+		}
+	}
+
+	/**
+	 * @param string $path
+	 * @param array $params
+	 *
+	 * @return RequestInterface
+	 */
+	private function prepareRequest($path, array $params) {
+		$payload   = \json_encode($params);
+		$uri       = $this->httpClient->createUri()
+			->withScheme('http')
+			->withHost($this->host)
+			->withPath($path)
+			->withQuery(
+				'api_key=' . \urlencode($this->apiKey) .
+				'&timestamp=' . \urlencode($this->timeProvider->getTimestamp())
+			);
+		$signature = \hash_hmac('MD5', $uri->getPath() . '?' . $uri->getQuery() . $payload, $this->apiSecret);
+		$uri       = $uri->withQuery($uri->getQuery() . '&signature=' . \urlencode($signature));
+
+		return $this->httpClient->createRequest()
+			->withUri($uri)
+			->withMethod('POST')
+			->withHeader('Content-Type', 'text/json')
+			->withBody($this->httpClient->createStringStream($payload));
+	}
 
 	/**
 	 * @param HTTPClientAdapter $httpClient
@@ -67,21 +111,8 @@ class HTTPRequestClient implements RequestClient {
 	 * @throws InvalidResponseData
 	 */
 	public function request($uri, array $params) {
-		$payload   = \json_encode($params);
-		$uri       = $this->httpClient->createUri()
-		                              ->withScheme('https')
-		                              ->withHost($this->host)
-		                              ->withPath($uri)
-		                              ->withQuery(
-			                              'api_key=' . \urlencode($this->apiKey) .
-			                              '&timestamp=' . \urlencode($this->timeProvider->getTimestamp()));
-		$signature = \hash_hmac('MD5', $uri->getPath() . '?' . $uri->getQuery() . $payload, $this->apiSecret);
-		$uri       = $uri->withQuery($uri->getQuery() . '&signature=' . \urlencode($signature));
-		$request   = $this->httpClient->createRequest()
-		                              ->withUri($uri)
-		                              ->withMethod('POST')
-		                              ->withHeader('Content-Type', 'text/json')
-		                              ->withBody($this->httpClient->createStringStream($payload));
+
+		$request = $this->prepareRequest($uri, $params);
 
 		$response = $this->httpClient->send($request);
 
@@ -92,5 +123,23 @@ class HTTPRequestClient implements RequestClient {
 		}
 
 		throw new InvalidResponseData($response->getBody());
+	}
+
+	/**
+	 * @param string $uri
+	 * @param array $params
+	 * @param mixed $stream
+	 *
+	 * @return StreamInterface
+	 */
+	public function requestStream($uri, array $params, $stream) {
+
+		$request = $this->prepareRequest($uri, $params);
+
+		$response = $this->getHttpClientEx()->sendAdvanced($request, [
+			HTTPClientAdapterEx::OPTION_RESPONSE_BODY_STREAM => $stream,
+		]);
+
+		return $response->getBody();
 	}
 }
---
+++ src/Request/RequestClientEx.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ixolit\Dislo\Request;
+
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Interface for clients to call Dislo.
+ *
+ * @package Dislo
+ */
+interface RequestClientEx {
+	/**
+	 * @param string $uri
+	 * @param array $params
+	 * @param mixed $stream
+	 *
+	 * @return StreamInterface
+	 */
+	public function requestStream($uri, array $params, $stream);
+}
