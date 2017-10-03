6702e4e40f9d396ed8628e2fce6a0bca55748a9e
dschobert <d.schobert@ixolit.com>
2017-10-03 13:26:56 +0200

    recurring price for package change price

--- src/Response/SubscriptionCalculatePackageChangeResponse.php
+++ src/Response/SubscriptionCalculatePackageChangeResponse.php
@@ -4,33 +4,30 @@ namespace Ixolit\Dislo\Response;
 
 use Ixolit\Dislo\WorkingObjects\Price;
 
-class SubscriptionCalculatePackageChangeResponse {
+class SubscriptionCalculatePackageChangeResponse extends SubscriptionPriceResponse {
 	/**
 	 * @var bool
 	 */
 	private $needsBilling;
 
 	/**
-	 * @var Price
-	 */
-	private $price;
-
-	/**
 	 * @var bool
 	 */
 	private $appliedImmediately;
 
-	/**
-	 * SubscriptionCalculateAddonPriceResponse constructor.
-	 *
-	 * @param bool  $needsBilling
-	 * @param Price $price
-	 * @param bool  $appliedImmediately
-	 */
-	public function __construct($needsBilling, Price $price, $appliedImmediately) {
+    /**
+     * SubscriptionCalculateAddonPriceResponse constructor.
+     *
+     * @param bool       $needsBilling
+     * @param Price      $price
+     * @param bool       $appliedImmediately
+     * @param Price|null $recurringPrice
+     */
+	public function __construct($needsBilling, Price $price, $appliedImmediately, Price $recurringPrice = null) {
 		$this->needsBilling = $needsBilling;
-		$this->price        = $price;
 		$this->appliedImmediately = $appliedImmediately;
+
+		parent::__construct($price, $recurringPrice);
 	}
 
 	/**
@@ -38,13 +35,6 @@ class SubscriptionCalculatePackageChangeResponse {
 	 */
 	public function isNeedsBilling() {
 		return $this->needsBilling;
-	}
-
-	/**
-	 * @return Price
-	 */
-	public function getPrice() {
-		return $this->price;
 	}
 
 	/**
@@ -63,7 +53,10 @@ class SubscriptionCalculatePackageChangeResponse {
 		return new SubscriptionCalculatePackageChangeResponse(
 			$response['needsBilling'],
 			Price::fromResponse($response['price']),
-			$response['appliedImmediately']
+			$response['appliedImmediately'],
+            isset($response['recurringPrice'])
+                ? Price::fromResponse($response['recurringPrice'])
+                : null
 		);
 	}
 }
