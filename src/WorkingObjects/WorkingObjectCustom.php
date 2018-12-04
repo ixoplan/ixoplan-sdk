ad4c7104b6ada74499fec8e9e4fd71cc87960456
Markus Marchewa <m.marchewa@ixolit.com>
2018-12-04 13:38:08 +0100

    fixed WorkingObjectCustom parameter type

--- src/WorkingObjects/WorkingObjectCustom.php
+++ src/WorkingObjects/WorkingObjectCustom.php
@@ -4,6 +4,6 @@ namespace Ixolit\Dislo\WorkingObjects;
 
 class WorkingObjectCustom implements WorkingObjectCustomInterface {
 
-	public function setWorkingObject($object) {
+	public function setWorkingObject(WorkingObject $object) {
 	}
 }
