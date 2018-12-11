c1be59cb84dc8d7673c255281b4fe320f9f3febe
Markus Marchewa <m.marchewa@ixolit.com>
2018-12-11 10:42:57 +0100

    added InvalidWorkingObjectException

---
+++ src/Exceptions/InvalidWorkingObjectException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ixolit\Dislo\Exceptions;
+
+use Ixolit\CDE\Exceptions\CDEException;
+
+/**
+ * Class InvalidWorkingObjectException
+ *
+ * @package Ixolit\CDE\Exceptions
+ */
+class InvalidWorkingObjectException extends \Exception implements CDEException {
+
+}
